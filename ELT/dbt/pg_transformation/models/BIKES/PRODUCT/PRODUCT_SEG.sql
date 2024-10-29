a7a7cda7cd946b3c93f77647d2941f0e47bd6d9e
Daniel Abraham R <abrahamsamuel3310@gmail.com>
2024-10-29 10:59:20 +0530

    Update: Query Logic

--- ELT/dbt/pg_transformation/models/BIKES/PRODUCT/PRODUCT_SEG.sql
+++ ELT/dbt/pg_transformation/models/BIKES/PRODUCT/PRODUCT_SEG.sql
@@ -1,5 +1,19 @@
-{{ config(alias=get_table_name(), schema = var('DATABASES')['target'] , enabled=enable()) }}
+{{ config(alias=get_table_name(), schema=var('DATABASES')['target'], enabled=enable()) }}
 
-
-select * from
- {{ source('staging', 'products') }}
+SELECT
+    a.productid AS p_id,
+    d.short_descr AS product_name,
+    a.prodcategoryid AS p_Cat_id,
+    c.short_descr AS category_name,
+    to_date({{ cast('ChangeDat','varchar') }}, 'yyyyMMdd') as changedDate
+FROM
+    {{ source('staging', 'products') }} a
+INNER JOIN
+    {{ source('staging', 'productcategories') }} b
+    ON a.prodcategoryid = b.prodcategoryid
+INNER JOIN
+    {{ source('staging', 'productcategorytext') }} c
+    ON b.prodcategoryid = c.prodcategoryid
+INNER JOIN
+    {{ source('staging', 'producttexts') }} d
+    ON a.productid = d.productid
